c33cf63ce85415c5a3bb2eb50a2edc24e035f405
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-10-15 12:07:12 +0200

    Initial Commit

---
+++ src/main/java/com/rallerenp/recodeblog/RecodeBlogApplication.java
@@ -0,0 +1,13 @@
+package com.rallerenp.recodeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecodeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecodeBlogApplication.class, args);
+    }
+
+}
